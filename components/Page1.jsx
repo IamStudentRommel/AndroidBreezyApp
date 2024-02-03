b1281ea197f437a3d7c2b74589f9ed6d5ddf60bc
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-02-02 22:04:34 -0700

    test

--- components/Page1.jsx
+++ components/Page1.jsx
@@ -1,24 +1,68 @@
-import {Button, StyleSheet, View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React from 'react';
+import {StyleSheet, View, Image} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 
-function Page2({navigation}) {
-  let location = {
-    latitude: 51.049999,
-    longitude: -114.066666,
-    latitudeDelta: 0.009,
-    longitudeDelta: 0.009,
+function Page1({navigation}) {
+  const initialLocation = {
+    latitude: 51.059086,
+    longitude: -113.981448,
+    latitudeDelta: 0.003,
+    longitudeDelta: 0.003,
   };
+
+  const markerLocations = [
+    {
+      id: 1,
+      title: 'Zombie-XXW133',
+      description: 'Semi Normal',
+      coordinate: {latitude: 51.059086, longitude: -113.981148},
+      icon: require('../assets/zombie.png'),
+    },
+    {
+      id: 2,
+      title: 'Zombie-GDS235',
+      description: 'Fake Soldier',
+      coordinate: {latitude: 51.059086, longitude: -113.981248},
+      icon: require('../assets/zombie.png'),
+    },
+    {
+      id: 3,
+      title: 'Zombie-ZSX125',
+      description: 'Grand Fleet',
+      coordinate: {latitude: 51.059086, longitude: -113.181148},
+      icon: require('../assets/zombie.png'),
+    },
+    {
+      id: 4,
+      title: 'Zombie-ABC123',
+      description: 'Drunken Master',
+      coordinate: {latitude: 51.059133, longitude: -113.982038},
+      icon: require('../assets/zombie.png'),
+    },
+  ];
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <MapView
         style={StyleSheet.absoluteFillObject}
         provider={PROVIDER_GOOGLE}
         mapType="hybrid"
-        region={location}></MapView>
-
-      {/* <Button onPress={() => navigation.goBack()} title="Go back login" /> */}
+        region={initialLocation}>
+        {/* Markers */}
+        {markerLocations.map(marker => (
+          <Marker
+            key={marker.id}
+            coordinate={marker.coordinate}
+            title={marker.title}
+            description={marker.description}>
+            {marker.icon && (
+              <Image source={marker.icon} style={{width: 32, height: 32}} />
+            )}
+          </Marker>
+        ))}
+      </MapView>
     </View>
   );
 }
 
-export default Page2;
+export default Page1;
