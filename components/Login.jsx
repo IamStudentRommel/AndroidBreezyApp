a7b1454a39e2f88b26fa5c994f90e2e89d3aca9f
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-02-03 20:39:42 -0700

    use expo

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,113 +1,88 @@
 import React from 'react';
-
 import {
-  StyleSheet,
-  Text,
   View,
+  KeyboardAvoidingView,
   TextInput,
-  TouchableOpacity,
+  StyleSheet,
   Image,
+  Platform,
+  TouchableWithoutFeedback,
+  Button,
+  Keyboard,
+  Text,
+  TouchableOpacity,
+  Alert,
 } from 'react-native';
 
-export default class Login extends React.Component {
-  state = {
-    email: '',
-    password: '',
+const Login = () => {
+  const test = () => {
+    // Handle the click for "Forgot Password?"
+    Alert.alert('feature not yet implemented');
+    // You can implement the logic for handling the forgot password functionality here
   };
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image source={require('../assets/logo.png')} style={styles.logo} />
-        <Text style={styles.appName}>Str8Watch</Text>
-        <Text style={styles.subAppName}>"All is Well"</Text>
-        <View style={styles.inputView}>
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <Image source={require('../assets/logo.png')} style={styles.logo} />
+          <Text style={styles.appName}>Str8Watch</Text>
+          <Text style={styles.subAppName}>"All is Well"</Text>
+          <TextInput placeholder="Username" style={styles.textInput} />
           <TextInput
-            style={styles.inputText}
-            placeholder="Email..."
-            placeholderTextColor="#003f5c"
-            onChangeText={text => this.setState({email: text})}
-          />
-        </View>
-        <View style={styles.inputView}>
-          <TextInput
+            placeholder="Password"
+            style={styles.textInput}
             secureTextEntry
-            style={styles.inputText}
-            placeholder="Password..."
-            placeholderTextColor="#003f5c"
-            onChangeText={text => this.setState({password: text})}
           />
+          <Button title="Forgot Password?" onPress={test} color="#3333ff" />
+          <View style={styles.btnContainer}>
+            <Button title="Login" onPress={test} color="#ffffff" />
+          </View>
+          <Button title="Signup" onPress={test} color="#3333ff" />
         </View>
-        <TouchableOpacity>
-          <Text style={styles.forgot}>Forgot Password?</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.signUp}>Signup</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  inner: {
+    padding: 24,
+    flex: 1,
+    justifyContent: 'space-around',
   },
   logo: {
-    width: 280,
+    width: 300,
     height: 250,
   },
   appName: {
     fontWeight: 'bold',
-    justifyContent: 'center',
+    textAlign: 'center',
     fontSize: 40,
-    color: '#ffffff',
-    // marginBottom: 40,
   },
   subAppName: {
     fontSize: 10,
-    marginBottom: 40,
     fontSize: 15,
-    color: '#ffffff',
+    textAlign: 'center',
     fontStyle: 'italic',
   },
-  inputView: {
-    width: '80%',
-    backgroundColor: '#ffffff',
-    borderRadius: 25,
-    height: 50,
-    marginBottom: 20,
-    justifyContent: 'center',
-    padding: 20,
+  textInput: {
+    height: 40,
+    borderColor: '#000000',
+    borderBottomWidth: 1,
   },
-  inputText: {
-    height: 50,
-    color: '#003f5c',
+  btnContainer: {
+    backgroundColor: '#0000b3',
   },
-  forgot: {
-    color: '#3333ff',
+  textLink: {
+    color: '#000099',
     fontSize: 15,
-  },
-  loginBtn: {
-    width: '80%',
-    backgroundColor: '#0000e6',
-    borderRadius: 25,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 40,
-    marginBottom: 10,
-  },
-  loginText: {
-    color: '#ffffff',
-  },
-  signUp: {
-    color: '#3333ff',
-    fontSize: 15,
+    textAlign: 'center',
   },
 });
+
+export default Login;
--- components/Page1.jsx
+++ components/Page1.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {StyleSheet, View, Image} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import Zombies from '../data/locations.json';
 
 function Page1({navigation}) {
   const initialLocation = {
@@ -10,37 +11,6 @@ function Page1({navigation}) {
     longitudeDelta: 0.003,
   };
 
-  const markerLocations = [
-    {
-      id: 1,
-      title: 'Zombie-XXW133',
-      description: 'Semi Normal',
-      coordinate: {latitude: 51.059086, longitude: -113.981148},
-      icon: require('../assets/zombie.png'),
-    },
-    {
-      id: 2,
-      title: 'Zombie-GDS235',
-      description: 'Fake Soldier',
-      coordinate: {latitude: 51.059086, longitude: -113.981248},
-      icon: require('../assets/zombie.png'),
-    },
-    {
-      id: 3,
-      title: 'Zombie-ZSX125',
-      description: 'Grand Fleet',
-      coordinate: {latitude: 51.059086, longitude: -113.181148},
-      icon: require('../assets/zombie.png'),
-    },
-    {
-      id: 4,
-      title: 'Zombie-ABC123',
-      description: 'Drunken Master',
-      coordinate: {latitude: 51.059133, longitude: -113.982038},
-      icon: require('../assets/zombie.png'),
-    },
-  ];
-
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <MapView
@@ -49,15 +19,16 @@ function Page1({navigation}) {
         mapType="hybrid"
         region={initialLocation}>
         {/* Markers */}
-        {markerLocations.map(marker => (
+        {Zombies.map(marker => (
           <Marker
             key={marker.id}
             coordinate={marker.coordinate}
             title={marker.title}
             description={marker.description}>
-            {marker.icon && (
-              <Image source={marker.icon} style={{width: 32, height: 32}} />
-            )}
+            <Image
+              source={require('../assets/zombie.png')}
+              style={{width: 32, height: 32}}
+            />
           </Marker>
         ))}
       </MapView>
