ecfbc7d91550f00571f77af1e6d4af158491d2db
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-02-08 21:06:29 -0700

    group btn and add modal temp

--- components/Page2.jsx
+++ components/Page2.jsx
@@ -1,17 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, TouchableOpacity, Text, Image} from 'react-native';
+import {View, StyleSheet, Image, Button, TouchableOpacity} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import * as Location from 'expo-location';
+// import {Ionicons} from '@expo/vector-icons';
 import testdata from '../data/test.json';
 import mapCustomStyle from '../data/mapCustomStyle.json';
-import * as Location from 'expo-location';
 
 const Page2 = () => {
   const [incidents, setIncidents] = useState([]);
   const [initialLocation, setInitialLocation] = useState({
     latitude: 51.05011,
     longitude: -114.08529,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    latitudeDelta: 0.0022,
+    longitudeDelta: 0.0091,
   });
 
   const requestLocationPermission = async () => {
@@ -31,7 +32,7 @@ const Page2 = () => {
         const {coords} = await Location.getCurrentPositionAsync({
           accuracy: Location.Accuracy.High,
         });
-        console.log('coords');
+        // console.log('coords');
         setInitialLocation({
           latitude: coords.latitude,
           longitude: coords.longitude,
@@ -63,58 +64,94 @@ const Page2 = () => {
 
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        region={initialLocation}
-        customMapStyle={mapCustomStyle}>
-        <Marker coordinate={initialLocation} title="Your Initial Location">
-          <Image
-            source={require('../assets/angel.png')}
-            style={{width: 32, height: 32}}
-          />
-        </Marker>
-
-        {testdata.map(marker => {
-          if (marker.year === '2023') {
-            try {
-              const coordinates = {
-                latitude: marker.community_center_point.coordinates[1],
-                longitude: marker.community_center_point.coordinates[0],
-              };
-              // console.log(marker.id);
-              return (
-                <Marker
-                  key={marker.id}
-                  coordinate={coordinates}
-                  title={marker.category}
-                  x
-                  description={marker.id}>
-                  <Image
-                    source={require('../assets/zombie.png')}
-                    style={{width: 32, height: 32}}
-                  />
-                </Marker>
-              );
-            } catch (error) {
-              return null;
+      <View style={styles.mapContainer}>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          region={initialLocation}
+          customMapStyle={mapCustomStyle}>
+          <Marker coordinate={initialLocation} title="Your Initial Location">
+            <Image
+              source={require('../assets/angel.png')}
+              style={{width: 32, height: 32}}
+            />
+          </Marker>
+          {testdata.map(marker => {
+            if (marker.year === '2023') {
+              try {
+                const coordinates = {
+                  latitude: marker.community_center_point.coordinates[1],
+                  longitude: marker.community_center_point.coordinates[0],
+                };
+                return (
+                  <Marker
+                    key={marker.id}
+                    coordinate={coordinates}
+                    title={marker.category}
+                    x
+                    description={marker.id}>
+                    <Image
+                      source={require('../assets/zombie.png')}
+                      style={{width: 32, height: 32}}
+                    />
+                  </Marker>
+                );
+              } catch (error) {
+                return null;
+              }
             }
-          }
-        })}
-      </MapView>
+          })}
+        </MapView>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Button
+            title="Button 1"
+            onPress={() => console.log('Button 1 pressed')}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="Button 2"
+            onPress={() => console.log('Button 2 pressed')}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="Button 3"
+            onPress={() => console.log('Button 3 pressed')}
+          />
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  mapContainer: {
+    flex: 1,
+    ...StyleSheet.absoluteFillObject,
   },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingVertical: 10,
+    backgroundColor: 'lightgray',
+  },
+  button: {
+    flex: 1,
+    marginHorizontal: 5,
   },
 });
 
--- components/TestPage.jsx
+++ components/TestPage.jsx
@@ -1,69 +1,100 @@
-// import {View, Text} from 'react-native';
+// import React, {useState} from 'react';
+// import {View, Text, Button, StyleSheet} from 'react-native';
+// import * as Location from 'expo-location';
+
+// const requestLocationPermission = async () => {
+//   try {
+//     const {status} = await Location.requestForegroundPermissionsAsync();
+//     return status === 'granted';
+//   } catch (err) {
+//     console.error(err);
+//     return false;
+//   }
+// };
+
 // const About = () => {
+//   const [location, setLocation] = useState(null);
+
+//   const getLocation = async () => {
+//     const hasPermission = await requestLocationPermission();
+//     if (hasPermission) {
+//       try {
+//         const {coords} = await Location.getCurrentPositionAsync({
+//           accuracy: Location.Accuracy.High,
+//         });
+//         setLocation(coords);
+//       } catch (error) {
+//         console.error('Error getting location:', error);
+//         setLocation(null);
+//       }
+//     }
+//   };
+
 //   return (
-//     <View s>
-//       <Text>Shit text</Text>
+//     <View style={styles.container}>
+//       <View
+//         style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
+//         <Button title="Get Location" onPress={getLocation} />
+//       </View>
+//       {location && (
+//         <>
+//           <Text>Latitude: {location.latitude}</Text>
+//           <Text>Longitude: {location.longitude}</Text>
+//         </>
+//       )}
 //     </View>
 //   );
 // };
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
 // export default About;
 
-import React, {useState} from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
-import * as Location from 'expo-location';
+import React, {useState, useRef} from 'react';
+import {View, Text, StyleSheet, Button, SafeAreaView} from 'react-native';
+import BottomDrawer from 'react-native-animated-bottom-drawer';
 
-const requestLocationPermission = async () => {
-  try {
-    const {status} = await Location.requestForegroundPermissionsAsync();
-    return status === 'granted';
-  } catch (err) {
-    console.error(err);
-    return false;
-  }
-};
+const TestPage = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const bottomDrawerRef = useRef(null);
 
-const About = () => {
-  const [location, setLocation] = useState(null);
-
-  const getLocation = async () => {
-    const hasPermission = await requestLocationPermission();
-    if (hasPermission) {
-      try {
-        const {coords} = await Location.getCurrentPositionAsync({
-          accuracy: Location.Accuracy.High,
-        });
-        setLocation(coords);
-      } catch (error) {
-        console.error('Error getting location:', error);
-        setLocation(null);
-      }
-    }
+  const handleOpenDrawer = () => {
+    setIsDrawerOpen(true);
+    bottomDrawerRef.current?.open();
   };
 
   return (
-    <View style={styles.container}>
-      <View
-        style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
-        <Button title="Get Location" onPress={getLocation} />
-      </View>
-      {location && (
-        <>
-          <Text>Latitude: {location.latitude}</Text>
-          <Text>Longitude: {location.longitude}</Text>
-        </>
-      )}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="Open" onPress={handleOpenDrawer} />
+      <BottomDrawer
+        ref={bottomDrawerRef}
+        openOnMount
+        startUp={false} // Disable start up animation for better customization
+        onChangeVisibility={visible => setIsDrawerOpen(visible)}>
+        <View style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomDrawer>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 24,
+  },
+  contentContainer: {
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
 
-export default About;
+export default TestPage;
