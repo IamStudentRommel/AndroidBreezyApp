8d2621e0ea0ab3711ed160553521718d7c124d80
rcostillas <125596689+rcostillas@users.noreply.github.com>
2024-04-13 22:13:25 -0600

    updated test.json with 2024 data

--- components/Dashboard/Dashboard.jsx
+++ components/Dashboard/Dashboard.jsx
@@ -24,8 +24,8 @@ const Report = () => {
   const [incidents, setIncidents] = useState([]);
   const [sec, setSec] = useState({});
 
-  const options = ['2020', '2021', '2022', '2023'];
-  const [selectedYear, setSelectedYear] = useState('2023');
+  const options = ['2020', '2021', '2022', '2023', '2024'];
+  const [selectedYear, setSelectedYear] = useState('2024');
 
   const [initialLocation, setInitialLocation] = useState({
     latitude: 51.05011,
